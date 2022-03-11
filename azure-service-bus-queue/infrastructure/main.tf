848717fa727d73f5f7bc26c41f95d81a29cb2e90
Patil, Parikshit <parikshit.patil@siemens.com>
2022-03-11 17:59:28 +0530

    changed infra

--- azure-service-bus-queue/infrastructure/main.tf
+++ azure-service-bus-queue/infrastructure/main.tf
@@ -13,17 +13,22 @@ resource "azurerm_servicebus_namespace" "keda-namespace" {
 
 resource "azurerm_servicebus_queue" "keda-demoq" {
   name         = var.servicebus-queue-name
-  namespace_id = azurerm_servicebus_namespace.example.id
+  namespace_name = azurerm_servicebus_namespace.keda-namespace.name
+  resource_group_name = azurerm_resource_group.kedaq-rg.name
 
   enable_partitioning = true
 }
 
 resource "azurerm_servicebus_queue_authorization_rule" "queuerule" {
   name     = "queuerule"
-  queue_id = azurerm_servicebus_queue.keda-demoq.id
+  namespace_name = azurerm_servicebus_namespace.keda-namespace.name
+  queue_name = azurerm_servicebus_queue.keda-demoq.name
+  resource_group_name = azurerm_resource_group.kedaq-rg.name
 
   # As per KEDA docs,
   # Service Bus Shared Access Policy needs to be of type Manage.
   # Manage access is required for KEDA to be able to get metrics from Service Bus.
   manage = true
+  listen = true
+  send = true
 }
--- azure-service-bus-queue/infrastructure/output.tf
+++ azure-service-bus-queue/infrastructure/output.tf
@@ -1,7 +1,9 @@
 output "namespace_primary_connection_string" {
     value = azurerm_servicebus_namespace.keda-namespace.default_primary_connection_string
+    sensitive = true
 }
 
 output "queue_primary_connection_string" {
     value = azurerm_servicebus_queue_authorization_rule.queuerule.primary_connection_string
+    sensitive = true
 }
